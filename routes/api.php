2e08c757f60735d457b0d6ba5a37a5f861e1257a
JPCarvalho23 <135696827+JPCarvalho23@users.noreply.github.com>
2024-01-09 16:53:12 +0000

    commit

--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,7 @@ use App\Http\Controllers\StudentsController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
